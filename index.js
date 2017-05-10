a543cda98073d10f3e3170736719e0a14f407417
Joakim J. Rapp <joakim.rapp@viaplay.com>
2017-05-10 16:07:38 +0200

    removed file

--- index.js
+++
@@ -1,7 +0,0 @@
-const writer = require( '.' )( 'test-app' );
-const log = require( '@jrapp/log-emitter' ).log( 'testModule' );
-log.info( 'hej', 'hopp' );
-writer.absolute();
-log.info( 'hej', 'hopp' );
-log.trace( 'hej', 'hopp' );
-log.error( 'hej', 'hopp' );
