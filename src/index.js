0722420dd7ff22d3da17af0341e03f53862ef1e8
Joakim J. Rapp <joakim.rapp@viaplay.com>
2017-05-16 13:35:31 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 module.exports = ( application, levelsConfig = [ "*", "!>info", "!<fatal" ], log = console.log ) => {
 	const logger = require( './logger.js' );
-	logger( 'log', log )( { name: 'writer', level: 3, message: 'start', meta: new Date().toString() } );
-	try { require( '@jrapp/log-emitter' ).on( levelsConfig, logger( application, log ) ); } catch( err ) {
+	try {
+		require( '@jrapp/log-emitter' ).on( levelsConfig, logger( application, log ) );
+		logger( 'log', log )( { name: 'writer', level: 3, message: 'start', meta: new Date().toString() } );
+	} catch( err ) {
 		logger( 'log', log )( { name: 'writer', level: 0, message: 'Log emitter must be installed!' } ); }
 	return require( './display.js' );
 };
--- test/tests/unit/src/index.js.test.js
+++ test/tests/unit/src/index.js.test.js
@@ -2,5 +2,10 @@ require( '../../../helpers/unit.js' )( ( writer ) => {} )
 	.describe( 'something' )
 		.it( 'should initialize', ( assert, writer ) => new Promise( resolve =>
 			assert.ok( writer( 'test', undefined, () => resolve() ) ) ) )
+		.it( 'should log a fatal log if log-emitter is not installed', ( assert, writer ) => new Promise( resolve =>
+			assert.ok( writer( 'test', () => {}, ( time, name, message ) => {
+				assert.ok( message.indexOf( 'Log emitter must be installed!' ) >= 0 );
+				resolve();
+			} ) ) ) )
 		.done()
 	.done();
