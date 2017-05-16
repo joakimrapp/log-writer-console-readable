8797e4fb1fa7b1dc87eaf38ae09dc82f12d65fcf
Joakim J. Rapp <joakim.rapp@viaplay.com>
2017-05-16 08:38:30 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = ( application, levelsConfig = [ "*", "!>info", "!<fatal" ], log = console.log ) => {
 	require( './logger.js' )( 'log', log )( { name: 'writer', level: 3, message: 'start', meta: new Date().toString() } );
 	const logger = require( './logger.js' )( application, log );
-	require( '@jrapp/log-emitter' ).on( '*', logger );
+	require( '@jrapp/log-emitter' ).on( levelsConfig, logger );
 	return require( './display.js' );
 };
