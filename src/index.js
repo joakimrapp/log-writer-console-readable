7c1084ca629401bd153f75b1417b65e4b607c7fe
Joakim J. Rapp <joakimrapp@hotmail.com>
2017-05-16 13:20:48 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 module.exports = ( application, levelsConfig = [ "*", "!>info", "!<fatal" ], log = console.log ) => {
-	require( './logger.js' )( 'log', log )( { name: 'writer', level: 3, message: 'start', meta: new Date().toString() } );
-	const logger = require( './logger.js' )( application, log );
-	require( '@jrapp/log-emitter' ).on( levelsConfig, logger );
+	const logger = require( './logger.js' );
+	logger( 'log', log )( { name: 'writer', level: 3, message: 'start', meta: new Date().toString() } );
+	try { require( '@jrapp/log-emitter' ).on( levelsConfig, logger( application, log ) ); } catch( err ) {
+		logger( 'log', log )( { name: 'writer', level: 0, message: 'Log emitter must be installed!' } ); }
 	return require( './display.js' );
 };
